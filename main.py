0fe0a297e9f4943520891f01ca4704945fbc1d8e
Thomas Houtrique <thomas@MacBook-Air-de-Thomas.local>
2025-01-10 09:47:57 +0100

    fix question type

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from qcm import QCM
 from question import Question
 
-q1: Question = question("Quelle est la capitale de la France ?", ["Paris", "Londres", "Berlin"], 'a')
-q2: Question = question("Quelle est la capitale de l'Allemagne ?", ["Dublin", "Berlin", "Düsseldorf"], 'b')
-q3: Question = question("Quelle est la capitale de l'Irlande ?", ["Dublin", "Londres", "Manchester"], 'a')
-q4: Question = question("Quelle est la capitale de l'Italie ?", ["Rome", "Milan", "Naples"], 'a')
-q5: Question = question("Quelle est la capitale des Émirats Arabes Unis ?", ["Dubaï", "Abu Dhabi", "Charjah"], 'b')
-q6: Question = question("Quelle est la capitale des États-Unis ?", ["New York", "Washington DC", "Los Angeles"], 'b')
-q7: Question = question("Quelle est la capitale de la Pologne ?", ["Cracovie", "Varsovie", "Gdańsk"], 'b')
-q8: Question = question("Quelle est la capitale de la Russie ?", ["Saint-Pétersbourg", "Moscou", "Kazan"], 'b')
-q9: Question = question("Quelle est la capitale du Japon ?", ["Osaka", "Tokyo", "Kyoto"], 'b')
+q1: Question = Question("Quelle est la capitale de la France ?", ["Paris", "Londres", "Berlin"], 'a')
+q2: Question = Question("Quelle est la capitale de l'Allemagne ?", ["Dublin", "Berlin", "Düsseldorf"], 'b')
+q3: Question = Question("Quelle est la capitale de l'Irlande ?", ["Dublin", "Londres", "Manchester"], 'a')
+q4: Question = Question("Quelle est la capitale de l'Italie ?", ["Rome", "Milan", "Naples"], 'a')
+q5: Question = Question("Quelle est la capitale des Émirats Arabes Unis ?", ["Dubaï", "Abu Dhabi", "Charjah"], 'b')
+q6: Question = Question("Quelle est la capitale des États-Unis ?", ["New York", "Washington DC", "Los Angeles"], 'b')
+q7: Question = Question("Quelle est la capitale de la Pologne ?", ["Cracovie", "Varsovie", "Gdańsk"], 'b')
+q8: Question = Question("Quelle est la capitale de la Russie ?", ["Saint-Pétersbourg", "Moscou", "Kazan"], 'b')
+q9: Question = Question("Quelle est la capitale du Japon ?", ["Osaka", "Tokyo", "Kyoto"], 'b')
 q10: Question = Question("Quelle est la capitale de l'Australie ?", ["Sydney", "Canberra", "Melbourne"], 'b')
 
 questions: list = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
@@ -21,9 +21,10 @@ while not qcm.is_finished():
     for i, choice in enumerate(qcm.get_choices()):
         print(f"{chr(97 + i)}) {choice}")
     answer: str = input("Réponse: ")
+    print("\n")
     qcm.check_answer(answer)
 
-print(f"Votre score est de {qcm.get_score()}/10")
+print(f"⭐️ Vous avez obtenu {qcm.get_score()} points sur 10.")
 
 print("Les réponses étaient:")
 for i, question in enumerate(questions):
